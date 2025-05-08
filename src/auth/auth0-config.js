4bd98aafd121db4715737c6e76537a584b8376e7
ParthD-Web <parth.dhameliya@jeavio.com>
2025-05-08 10:09:38 +0530

    fix: sso and redirection

--- src/auth/auth0-config.js
+++ src/auth/auth0-config.js
@@ -1,15 +1,10 @@
 // auth0-config.js
 export const auth0Config = {
   domain: 'dev-nc7cdg4nhc8z8gi1.us.auth0.com',
-  clientId: 'SFwQLW5jijgxh6KydShvRLVBRMH85O0a',
-  redirectUri: window.location.origin,
+  clientId: 'uidgeWRN7e6NVtAxSWLjjYEMkhCwBDmf',
+  redirectUri: 'http://localhost:5173',
   audience: 'https://dev-nc7cdg4nhc8z8gi1.us.auth0.com/api/v2/',
   scope: 'openid profile email offline_access',
   useRefreshTokens: true,
   cacheLocation: 'localstorage',
-  // Properly configure logout parameters
-  logoutParams: {
-    returnTo: window.location.origin + '?logout=true',
-    clientId: 'SFwQLW5jijgxh6KydShvRLVBRMH85O0a',
-  },
 };
